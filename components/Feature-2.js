a03449cea67cd86faa4212514089da60cdcf66b5
NaseerFast <jaafar.nasir71@gmail.com>
2023-04-17 23:30:00 +0100

    updated feature-2

--- components/Feature-2.js
+++ components/Feature-2.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NextLink from 'next/link';;
+import NextLink from 'next/link';
 import Image from 'next/image';
 // import ImageNewLeg from '../public/images/YPImage.jpg'
 

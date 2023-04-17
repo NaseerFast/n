69a8f324ab0e34755d96eb87d713b32b0c0064dc
NaseerFast <jaafar.nasir71@gmail.com>
2023-04-17 23:17:07 +0100

    Update NewHero.js

--- components/NewHero.js
+++ components/NewHero.js
@@ -55,34 +55,34 @@ const NewHero = () => {
     <section id="hero">
     {/* <!-- Flex Container --> */}
     <div
-      class="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row"
+      className="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row"
     >
       {/* <!-- Left item --> */}
-      <div class="flex flex-col mt-12 mb-32 space-y-12 md:w-1/2">
+      <div className="flex flex-col mt-12 mb-32 space-y-12 md:w-1/2">
         <h1
-          class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left"
+          className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left"
         >
          Looking for a way to get your business noticed?
         </h1>
-        {/* <p class="max-w-sm text-center text-darkGrayishBlue md:text-left">
+        {/* <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
         Look no further than Yellow Pages Digital Enterprise! Our online platform makes it easy to list your business and get noticed by customers searching for your products or services.
         </p> */}
 
-        <ul class="max-w-lg space-y-4 text-neutral-800 list-inside ">
-            <li class="flex items-center">  <svg class="flex-shrink-0 w-7 h-6 text-gray-800 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg> Look no further than Yellow Pages Digital Enterprise!</li>
-            <li class="flex items-center"> <svg class="flex-shrink-0 w-7 h-6 text-gray-800 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>Our online platform makes it easy to list your business and get noticed by customers searching for your products or services.</li>
-            <li class="flex items-center"> <svg class="flex-shrink-0 w-7 h-6 text-gray-800 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>For an even more widespread reach, consider adding one of out outdoor billboards to your marketing mix!</li>
+        <ul className="max-w-lg space-y-4 text-neutral-800 list-inside ">
+            <li className="flex items-center"> <svg className="flex-shrink-0 w-7 h-6 text-gray-800 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg> Look no further than Yellow Pages Digital Enterprise!</li>
+            <li className="flex items-center"> <svg className="flex-shrink-0 w-7 h-6 text-gray-800 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>Our online platform makes it easy to list your business and get noticed by customers searching for your products or services.</li>
+            <li className="flex items-center"> <svg className="flex-shrink-0 w-7 h-6 text-gray-800 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>For an even more widespread reach, consider adding one of out outdoor billboards to your marketing mix!</li>
         </ul>
-        <div class="flex justify-center md:justify-start">
+        <div className="flex justify-center md:justify-start">
           <a
             href="#"
-            class="p-3 px-6 pt-2 text-gray-800 bg-yellow-400 rounded-full baseline bg-brightRedLight"
+            className="p-3 px-6 pt-2 text-gray-800 bg-yellow-400 rounded-full baseline bg-brightRedLight"
             >Get Started</a
           >
         </div>
       </div>
       {/* <!-- Image --> */}
-      <div class="md:w-1/2">
+      <div className="md:w-1/2">
         <img src="/images/illustration-intro.svg" alt="" />
       </div>
     </div>
@@ -91,14 +91,14 @@ const NewHero = () => {
     {/* <section id="features">
       /* <!-- Flex container -
       <div
-        class="container flex flex-col px-4 mx-auto mt-10 space-y-12 md:space-y-0 md:flex-row"
+        className="container flex flex-col px-4 mx-auto mt-10 space-y-12 md:space-y-0 md:flex-row"
       >
         {/* <!-- What's Different 
-        <div class="flex flex-col space-y-12 md:w-1/2">
-          <h2 class="max-w-md text-4xl font-bold text-center md:text-left">
+        <div className="flex flex-col space-y-12 md:w-1/2">
+          <h2 className="max-w-md text-4xl font-bold text-center md:text-left">
             What's different about Manage?
           </h2>
-          <p class="max-w-sm text-center text-darkGrayishBlue md:text-left">
+          <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
             Manage provides all the functionality your team needs, without the
             complexity. Our software is tailor-made for modern digital product
             teams.
@@ -106,30 +106,30 @@ const NewHero = () => {
         </div>
 
     
-        <div class="flex flex-col space-y-8 md:w-1/2">
+        <div className="flex flex-col space-y-8 md:w-1/2">
           
           <div
-            class="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row"
+            className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row"
           >
             
-            <div class="rounded-l-full bg-brightRedSupLight md:bg-transparent">
-              <div class="flex items-center space-x-2">
+            <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent">
+              <div className="flex items-center space-x-2">
                 <div
-                  class="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed"
+                  className="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed"
                 >
                   01
                 </div>
-                <h3 class="text-base font-bold md:mb-4 md:hidden">
+                <h3 className="text-base font-bold md:mb-4 md:hidden">
                   Track company-wide progress
                 </h3>
               </div>
             </div>
 
             <div>
-              <h3 class="hidden mb-4 text-lg font-bold md:block">
+              <h3 className="hidden mb-4 text-lg font-bold md:block">
                 Track company-wide progress
               </h3>
-              <p class="text-darkGrayishBlue">
+              <p className="text-darkGrayishBlue">
                 See how your day-to-day tasks fit into the wider vision. Go from
               tracking progress at the milestone level all the way down to the
                 smallest of details. Never lose sight of the bigger picture
@@ -140,27 +140,27 @@ const NewHero = () => {
 
       
           <div
-            class="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row"
+            className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row"
           >
         
-            <div class="rounded-l-full bg-brightRedSupLight md:bg-transparent">
-              <div class="flex items-center space-x-2">
+            <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent">
+              <div className="flex items-center space-x-2">
                 <div
-                  class="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed"
+                  className="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed"
                 >
                   02
                 </div>
-                <h3 class="text-base font-bold md:mb-4 md:hidden">
+                <h3 className="text-base font-bold md:mb-4 md:hidden">
                   Advanced built-in reports
                 </h3>
               </div>
             </div>
 
             <div>
-              <h3 class="hidden mb-4 text-lg font-bold md:block">
+              <h3 className="hidden mb-4 text-lg font-bold md:block">
                 Advanced built-in reports
               </h3>
-              <p class="text-darkGrayishBlue">
+              <p className="text-darkGrayishBlue">
                 Set internal delivery estimates and track progress toward
                 company goals. Our customisable dashboard helps you build out
                 the reports you need to keep key stakeholders informed.
@@ -170,27 +170,27 @@ const NewHero = () => {
 
         
           <div
-            class="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row"
+            className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row"
           >
             
-            <div class="rounded-l-full bg-brightRedSupLight md:bg-transparent">
-              <div class="flex items-center space-x-2">
+            <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent">
+              <div className="flex items-center space-x-2">
                 <div
-                  class="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed"
+                  className="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed"
                 >
                   03
                 </div>
-                <h3 class="text-base font-bold md:mb-4 md:hidden">
+                <h3 className="text-base font-bold md:mb-4 md:hidden">
                   Everything you need in one place
                 </h3>
               </div>
             </div>
 
             <div>
-              <h3 class="hidden mb-4 text-lg font-bold md:block">
+              <h3 className="hidden mb-4 text-lg font-bold md:block">
                 Everything you need in one place
               </h3>
-              <p class="text-darkGrayishBlue">
+              <p className="text-darkGrayishBlue">
                 Stop jumping from one service to another to communicate, store
                 files, track tasks and share documents. Manage offers an
                 all-in-one team productivity solution.
